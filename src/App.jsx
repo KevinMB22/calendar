c29fbe0b4343f415529b447ee4dcc42a5147139f
KevinMB22 <73042033+KevinMB22@users.noreply.github.com>
2024-01-08 00:36:49 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+
+import React, { useState } from 'react';
+
+export default function App() {
+
+  const data = [
+    {
+      id:1,
+      heure:'15:00',
+      duree: 90
+    }
+  ]
+
+  return (
+    <div className='w-full h-[100vh] flex justify-center items-center'>
+      <article className="w-[100%] h-[100%] flex items-center">
+        <figure className='w-[80px] h-[100%] flex flex-col justify-between items-center'>
+          <p>8h</p>
+          <p>9h</p>
+          <p>10h</p>
+          <p>11h</p>
+          <p>12h</p>
+          <p>13h</p>
+          <p>14h</p>
+          <p>15h</p>
+          <p>16h</p>
+          <p>17h</p>
+          <p>18h</p>
+          <p>19h</p>
+          <p>20h</p>
+          <p>21h</p>
+        </figure>
+        <figure className='w-full h-[100%] flex flex-col'>
+            <div className='w-full h-[90px] bg-slate-500'>
+              <p className='px-3 text-white'>ID 1</p>
+            </div>
+            <div className='w-full h-[90px] bg-red-500'>
+              <p className='px-3 text-white'>ID 1</p>
+            </div>
+        </figure>
+
+      </article>
+    </div>
+  );
+};
+
+
